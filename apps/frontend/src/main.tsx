ab7f1eb303bd5c6c1489f8db7d61aef9911a382d
Isaque Lins <isaquelins523@gmail.com>
2025-04-14 22:08:07 -0300

    Atualizando configurações globais

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,11 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-import { GlobalStyle } from "./styles/GlobalStyles.ts";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GlobalStyle />
     <App />
   </StrictMode>
 );
